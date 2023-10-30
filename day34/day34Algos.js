acabc255b70a738fd1a1262e418413f8457cd259
Kierra Call <142943885+Kierra-Call@users.noreply.github.com>
2023-10-30 12:20:32 -0400

    update

---
+++ day34/day34Algos.js
@@ -0,0 +1,34 @@
+// given two sorted arrays that may have duplicate values, merge them and remove any duplicates
+
+var arr1 = [1, 3, 3, 5, 8, 10];
+var arr2 = [1, 3, 3, 5, 8, 10, 10, 10];
+// [1, 3, 5, 8, 10]
+
+var arrA = [1, 3, 4, 5];
+var arrB = [1, 3, 3, 5, 8, 10];
+// [1, 3, 4]
+
+[][(1, 2, 2)];
+
+// [1,2]
+
+// 1. DRIVER 🚗
+// 2. PRESENTER 🧑‍🏫
+// 3. NAVIGATOR 🧭
+
+// create a function that takes 2 sorted arrays
+function mergeDedupe(arr1, arr2) {
+  //! write the pseudocode first!
+  //1. create a temp var for our output array
+  //2. ....
+  // create temp var
+  // merge arrays
+  // use bubblesort to get rid of dupes
+  //
+  // 10. return the output array
+  //! do not use Set()
+}
+
+// try out some other tests
+mergeDedupe([1, 3, 3, 5, 8, 10], [1, 3, 4, 5]); // [ 1, 3, 4, 5, 8, 10 ]
+mergeDedupe([2, 3, 3, 5, 8, 10, 12], [1, 3, 4, 6]); // [1, 2, 3, 4, 5, 6, 8, 10, 12]
