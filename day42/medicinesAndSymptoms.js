bd48a1b9933cb2c5ffda9ef1bfe0aaaefaa1beab
Kierra Call <142943885+Kierra-Call@users.noreply.github.com>
2023-11-10 12:59:44 -0500

    update

---
+++ day42/medicinesAndSymptoms.js
@@ -0,0 +1,108 @@
+/*
+  Given an array of ailments, and an array of medication objects that have a nested array of treatedSymptoms
+  return the medication name(s) that treats the most given symptoms
+*/
+
+// RIOT
+
+// HINTS:
+// Loop through first array to check the medicine
+// then loop through second array to see if the symptoms match the inputted symptoms
+// if they match add them into a new array at the end return array
+// edge case if no matches return null
+const medications = [
+    {
+        name: "Sulforaphane",
+        treatableSymptoms: [
+            "dementia",
+            "alzheimer's",
+            "inflammation",
+            "neuropathy",
+        ],
+    },
+    {
+        name: "Longvida Curcumin",
+        treatableSymptoms: [
+            "pain",
+            "inflammation",
+            "depression",
+            "arthritis",
+            "anxiety",
+        ],
+    },
+    {
+        name: "Hericium erinaceus",
+        treatableSymptoms: [
+            "anxiety",
+            "cognitive decline",
+            "depression"],
+    },
+    {
+        name: "Nicotinamide mononucleotide",
+        treatableSymptoms: [
+            "ageing",
+            "low NAD",
+            "obesity",
+            "mitochondrial myopathy",
+            "diabetes",
+        ],
+    },
+    {
+        name: "PainAssassinator",
+        treatableSymptoms: [
+            "pain",
+            "inflammation",
+            "cramps",
+            "headache",
+            "toothache",
+            "back pain",
+            "fever",
+        ],
+    },
+];
+/*
+Input: ["pain"], medications
+Output: ["PainAssassinator", "Longvida Curcumin"]
+*/
+
+/*
+Input: ["pain", "inflammation", "depression"], medications
+Output: ["Longvida Curcumin"]
+*/
+
+/*
+Input: ["existential dread"], medications
+Output: []
+*/
+
+// HINTS:
+// Loop through first array to check the medicine
+// then loop through second array to see if the symptoms match the inputted symptoms
+// if they match add them into a new array at the end return array
+// edge case if no matches return null
+
+function webMD(ailments, meds) { 
+    let result = []
+    let max = 1
+    for (const med of meds) {
+        const matches = ailments.filter((ailment) => {
+            return med.treatableSymptoms.includes(ailment)
+        }).length
+        if (matches > max) {
+            max = matches
+            result = []
+            result.push(med.name)
+        }
+        else if (matches === max) {
+            result.push(med.name);
+        }
+    }
+    return result;
+}
+
+console.log(webMD(["pain"], medications));
+console.log(webMD(["pain", "inflammation", "depression"], medications));
+console.log(webMD(["existential dread"], medications));
+
+//psudocode
+//
