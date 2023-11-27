0e96adacf129eb04e965fd0fc2b74e1ea4d577fa
Kierra-Call <kierra.n.call@gmail.com>
2023-11-27 16:31:04 -0500

    Update

---
+++ day44/SinglyLinkedList.js
@@ -0,0 +1,144 @@
+/**
+ * A class to represents a single item of a SinglyLinkedList that can be
+ * linked to other Node instances to form a list of linked nodes.
+ */
+class ListNode {
+    /**
+     * Constructs a new Node instance. Executed when the 'new' keyword is used.
+     * @param {any} data The data to be added into this new instance of a Node.
+     *    The data can be anything, just like an array can contain strings,
+     *    numbers, objects, etc.
+     * @returns {ListNode} A new Node instance is returned automatically without
+     *    having to be explicitly written (implicit return).
+     */
+    constructor(data) {
+        this.data = data;
+        /**
+         * This property is used to link this node to whichever node is next
+         * in the list. By default, this new node is not linked to any other
+         * nodes, so the setting / updating of this property will happen sometime
+         * after this node is created.
+         *
+         * @type {ListNode|null}
+         */
+        this.next = null;
+    }
+}
+
+/**
+ * This class keeps track of the start (head) of the list and to store all the
+ * functionality (methods) that each list should have.
+ */
+class SinglyLinkedList {
+    /**
+     * Constructs a new instance of an empty linked list that inherits all the
+     * methods.
+     * @returns {SinglyLinkedList} The new list that is instantiated is implicitly
+     *    returned without having to explicitly write "return".
+     */
+    constructor() {
+        /** @type {ListNode|null} */
+        this.head = null; //Beginning, only one start.
+        //When next is null is when you're at the end of a list
+    }
+
+    /**
+     * Determines if this list is empty.
+     * - Time: O(?).
+     * - Space: O(?).
+     * @returns {boolean}
+     */
+    isEmpty() {
+        return this.head === null
+        // if (this.head === null)
+        //     return true
+
+        // return false
+    }
+
+    /**
+     * Creates a new node with the given data and inserts it at the back of
+     * this list.
+     * - Time: O(?).
+     * - Space: O(?).
+     * @param {any} data The data to be added to the new node.
+     * @returns {SinglyLinkedList} This list.
+     */
+    insertAtBack(data) {
+        const newNode = new ListNode(data)
+
+        if (this.isEmpty()) {
+            this.head = newNode
+            return
+        }
+
+        let runner = this.head
+
+        while (runner.next !== null) {
+            runner = runner.next
+        }
+        runner.next = newNode
+    }
+
+    /**
+     * Creates a new node with the given data and inserts it at the back of
+     * this list.
+     * - Time: O(?).
+     * - Space: O(?).
+     * @param {any} data The data to be added to the new node.
+     * @param {?ListNode} runner The current node during the traversal of this list
+     *    or null when the end of the list has been reached.
+     * @returns {SinglyLinkedList} This list.
+     */
+    insertAtBackRecursive(data, runner = this.head) {
+        if (runner.next === null) {
+            runner.next = new ListNode(data)
+            return
+        }
+
+        this.insertAtBackRecursive(data, runner.next)
+    }
+
+    /**
+     * Calls insertAtBack on each item of the given array.
+     * - Time: O(n * m) n = list length, m = arr.length.
+     * - Space: O(1) constant.
+     * @param {Array<any>} vals The data for each new node.
+     * @returns {SinglyLinkedList} This list.
+     */
+    insertAtBackMany(vals) {
+        for (const item of vals) {
+            this.insertAtBack(item);
+        }
+        return this;
+    }
+
+    /**
+     * Converts this list into an array containing the data of each node.
+     * - Time: O(n) linear.
+     * - Space: O(n).
+     * @returns {Array<any>} An array of each node's data.
+     */
+    toArr() {
+        const arr = [];
+        let runner = this.head;
+
+        while (runner) {
+            arr.push(runner.data);
+            runner = runner.next;
+        }
+        return arr;
+    }
+}
+
+// *******************************************************************
+// Test Code Here ~ ☕
+const singlyLinkedList = new SinglyLinkedList();
+
+console.log(SinglyLinkedList.isEmpty())
+SinglyLinkedList.insertAtBack(5)
+SinglyLinkedList.insertAtBack(6)
+console.log(singlyLinkedList.toArr())
+
+
+// Fun fact: Many video games use Singly Linked Lists for Inventory Management and, engines like Unity, can sometimes use them to manage key frames for animation under the hood. :)
---
+++ info.js
@@ -0,0 +1,5 @@
+const array = [1,2,3,"hey there",5]
+
+for (let i = 0; i < array.length; i++) {
+    console.log(array[i])
+}
